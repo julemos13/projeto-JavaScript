318c46dd534b804a5f23c7f27f06499ea5ab564e
julianaLemos <julemosflores@gmail.com>
2023-10-05 15:49:45 -0300

    ok

---
+++ aula 2/script.js
@@ -0,0 +1,40 @@
+// tipos primitivos
+
+// boolean
+/* var vOUf = false;
+console.log(vOuF);
+
+// number 
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer)
+
+// string
+var nome= 'juliana';
+console.log(typeof(nome));
+
+// function
+var funcao = function(){}
+console.log(typeof(funcao)); 
+
+// como declarar
+var variavel = 'juliana';
+variavel = 'martine';
+console.log(variavel);
+
+let variavel2 = 'juliana'
+variavel2 = 'martine';
+console.log(variavel2);
+
+const constante =1juliana';
+constante = 'martine';
+console.log(constante)
+
+var escopoGobla = 'global';
+console.log(escopoGlobal);
+
+function escopoLocal() {
+    let escopoLocalInterno = 'local';
+    consile.log(escopoLocalInterno);
+}
+
+escopolocal();
